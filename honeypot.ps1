8d0dd7c27f7b0f13f0e45abfda9e2a9b66bdc63b
Aholynomic <ethanchastie@gmail.com>
2022-03-26 12:56:41 +0000

    added initial user details

--- honeypot.ps1
+++ honeypot.ps1
@@ -1,34 +1,139 @@
 <#
-Script created by E. Hastie to automate insertion of 
-honey users in powershell
+.SYNOPSIS
+A script to insert honey users into the generated AD Badblood Environment, provided as part of dissertation work. 
+
+.DESCRIPTION
+Created using PowerShell, the script inserts 5 honey users using set variables.
+
+Author: Ethan Hastie
 #>
 
-Import-Module ActiveDirectory -EA Stop
+Import-Module ActiveDirectory
 
 function Show-Menu {
+    <#
+    .DESCRIPTION
+    Created using PowerShell, provides an interactive main menu.
+    #>
+
     Clear-Host
     Write-Host "============ HoneyAD ==============="
     Write-Host 
-    Write-Host "1: Press '1' to create user."
+    Write-Host "1: Press '1' to create the users."
     Write-Host "2: Press '2' for description."
     Write-Host "Q: Press 'Q' to quit."
     Write-Host
+}
+
+function Specify-Password {
+    <#
+    .DESCRIPTION
+    Create a password for the user according to length and complexity requirements
+    #>
+
+    # TOGGLE ME - in this case, the minimum length is 7 chars
+    $PasswordLength =         7
+
+    do {
+
+        Write-Host
+            $isGood = 0
+            $Password = Read-Host "Enter the Password: " -AsSecureString
+            $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
+            $Complexity = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+ 
+            if ($Complexity.Length -ge $PasswordLength) {
+                Write-Host
+            } else {
+                Write-Host "Password needs $PasswordLength or more Characters" -ForegroundColor:Green
+            }
+        
+            if ($Complexity -match "[^a-zA-Z0-9]") {
+                $isGood++
+            } else {
+                Write-Host "Password does not contain Special Characters." -ForegroundColor:Green
+            }
+        
+            if ($Complexity -match "[0-9]") {
+                $isGood++
+            } else {
+                Write-Host "Password does not contain Numbers." -ForegroundColor:Green
+            }
+        
+            if ($Complexity -cmatch "[a-z]") {
+                $isGood++
+            } else {
+                Write-Host "Password does not contain Lowercase letters." -ForegroundColor:Green
+            }
+        
+            if ($Complexity -cmatch "[A-Z]") {
+                $isGood++
+            } else {
+                Write-Host "Password does not contain Uppercase letters." -ForegroundColor:Green
+            }
+    } until ($Password.Length -ge $PasswordLength -and $isGood -ge 3)
+
+    return $Password
 }
 
 function Modify-Groups {
     $OU = "DC=hast,DC=interal"
 }
 
-function Create-User {
+function Create-Users {
+    <#
+    .DESCRIPTION
     
+    #>
+
     # leave firstname blank because of the structure of badblood
     # First Name: 'blank'
     # Surname: 'JOHN_SMITH'
-    $FirstName = ""
-    $Surname = Read-Host "Enter Last Name"
+    # Domain is same across all accounts
+    $FirstName =            ""
+    $Domain =               $env:userdnsdomain
+
+    # create first user that is domain admin
+    # and can add members to this group
+    Write-Host 
+    Write-Host "======== First User Attributes ========="
+    Write-Host "- Domain Admin"
+    Write-Host "- Add Users to Domain Admin Group"
+    Write-Host "- Can't Logon to Any Workstations"
+    Write-Host 
+
+    $Surname = Read-Host "Enter Last Name: "
+    $Description = Read-Host "Enter the User's Description: "
     $FullName = "$FirstName $Surname"
     $LogonName = $FullName
-    $Password = Read-Host "Enter a Password" | ConvertTo-SecureString -AsPlainText -Force
+    $Password = Specify-Password()
+
+    New-ADUser `
+    - Name $FullName
+    - GivenName $Firstname
+    - Surname $Surname
+    - SamAccountName $LogonName
+    - UserPrincipalName $LogonName@$Domain
+    - Displayname $FullName
+    - LogonWorkstations $null
+    - Path 
+    - AccountPassword $Password
+    - Enabled $true
+    - PasswordNeverExpires $true
+
+    Write-Host "======================================="
+    Write-Host
+    Write-Host "Firstname:                   $Firstname"
+    Write-Host "Lastname:                      $Surname"
+    Write-Host "Display Name:                 $FullName"
+    Write-Host "Logon Name:                  $LogonName"
+    Write-Host "OU:                                 $OU"
+    Write-Host "Domain:                         $Domain"
+    Write-Host "Password:                     $Password"
+    Write-Host "Credential - Not Expire:             No"
+    Write-Host "Enabled:                            Yes"
+    Write-Host "Logon Workstation/s:               NULL"
+    Write-Host
 }
 
 function Main {
